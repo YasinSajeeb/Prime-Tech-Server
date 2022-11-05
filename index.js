0e8eb6f214f6c4408ed6b67b1a80f34885f86d7c
YasinSajeeb <yeasinalhossen@gmail.com>
2022-11-05 09:06:07 +0600

    categories data set

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./Data/categories.json');
+
 app.get('/', (req, res) =>{
     res.send('Wow!!Courses api running');
 });
+
+app.get('/courses-categories', (req, res) =>{
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('Prime Tech server running on port', port);
