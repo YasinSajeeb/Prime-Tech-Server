b7c825b4ffb89ca33be80bc0578e6f39e3ca7117
YasinSajeeb <yeasinalhossen@gmail.com>
2022-11-10 02:58:10 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./Data/categories.json');
 
 const courses = require('./Data/courses.json');
 
@@ -13,14 +12,16 @@ app.get('/', (req, res) =>{
     res.send('Wow!!Courses api running');
 });
 
-app.get('/courses-categories', (req, res) =>{
-    res.send(categories)
+app.get('/courses', (req, res) =>{
+    res.send(courses);
 })
 
-app.get('/courses', (req, res) =>{
-    res.send(courses)
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const course = courses.find(course => course.id===id);
+    res.send(course);
 })
 
 app.listen(port, () => {
-    console.log('Prime Tech server running on port', port);
+    console.log(`Prime Tech server running on port, ${port}`);
 })
